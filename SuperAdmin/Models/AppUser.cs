98e69e0759517cc25febf2fa056f51630384b630
brunoHao <106672020+brunoHao@users.noreply.github.com>
2022-10-22 12:31:28 -0700

    FinishLoginWithController

---
+++ SuperAdmin/Models/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SuperAdmin.Models
+{
+    public class AppUser : IdentityUser
+    {
+    }
+}
---
+++ SuperAdmin/Models/MyDatabase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SuperAdmin.Models
+{
+    public class MyDatabase :IdentityDbContext<AppUser>
+    {
+        //options sẽ đc DI inject khi dịch vụ Myblogcontext được tạo ra.
+        public MyDatabase(DbContextOptions<MyDatabase> options) : base(options)
+        {
+
+        }
+        //luôn luôn nạp chồng OnConfiguring . nó sẽ hoạt động khi MyBlogContext được tạo ra.
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            base.OnConfiguring(builder);
+        }
+
+        //phương thức thường đucợ nạp chồng.
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            base.OnModelCreating(model);
+        }
+    }
+}
